9bfda0c8be8ea3ee6e7a6f1b1c1449543d2c56a1
dileepk706 <dlpkmr706@gmail.com>
2023-08-05 21:42:40 +0530

    add schedule for doctor started

--- src/routes/patient/PatientProtected.ts
+++ src/routes/patient/PatientProtected.ts
@@ -1,14 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAppSelector } from '../../redux/hooks';
 
 interface patientProtectedProps {
   children: React.ReactElement; // or React.ReactNode if you want to allow any type of children
 }
 
 const PatientProtected: React.FC<patientProtectedProps> = ({ children }) => {
-  const token = true; // TypeScript will infer the type based on the assigned value
-
-  if (token) { // You can use the boolean value directly for the condition
+  const userToken = useAppSelector(state=>state.user.accessToken)
+  
+  if (userToken) {// You can use the boolean value directly for the condition
     return children;
   } else {
     Navigate({to:"/login"})
