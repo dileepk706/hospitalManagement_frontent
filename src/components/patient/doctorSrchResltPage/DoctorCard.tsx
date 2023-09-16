6300dc59ac27f9fdf7f44e3f95cfc003dd2d71be
dileepk706 <dlpkmr706@gmail.com>
2023-09-16 18:28:25 +0530

    change

--- src/components/patient/doctorSrchResltPage/DoctorCard.tsx
+++ src/components/patient/doctorSrchResltPage/DoctorCard.tsx
@@ -1,7 +1,7 @@
 import { Send } from "@mui/icons-material"
 import { Button, Chip, Rating } from "@mui/material"
 import React from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Review } from "../../../types/Models"
 
 type DoctorCardProps = {
@@ -23,13 +23,16 @@ const DoctorCard: React.FC<DoctorCardProps> = ({ DoctorName, department, image, 
     const defualtImage = "https://img.freepik.com/premium-vector/avatar-female-doctor-with-black-hair-doctor-with-stethoscope-vector-illustrationxa_276184-33.jpg?w=2000"
 
     const reviewCount = review?.length;
+    const navigate=useNavigate()
     return (
-        <Link to={`/make-appointment/${id}`}>
 
         
         <div v-for="card in cards" className="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-[0_5px_15px_rgba(8,_112,_184,_0.7)]  mt-4 w-100 ">
             <div className="h-64 w-auto md:w-1/3 p-3">
                 <img
+                onClick={()=>{
+                    navigate(`/make-appointment/${id}`)
+                } }
                     src={image ? image : defualtImage} alt=""
                     className="inset-0 h-full w-full object-cover object-center rounded-[17px] shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]" />
             </div>
@@ -94,7 +97,6 @@ const DoctorCard: React.FC<DoctorCardProps> = ({ DoctorName, department, image, 
             </div>
 
         </div>
-        </Link>
     )
 }
 
